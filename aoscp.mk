0bffa1c654bc5eaba680415295288f7593b14124
georgemath24 <georgemannil2000@gmail.com>
2017-03-28 17:26:25 +0530

    Update aoscp.mk

--- aoscp.mk
+++ aoscp.mk
@@ -18,8 +18,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from onyx device
 $(call inherit-product, device/oneplus/onyx/device.mk)
 
-# Inherit some common Bullshit.
-$(call inherit-product, vendor/aoscp/common.mk)
+# Inherit some common Cosmic stuff.
+$(call inherit-product, vendor/aoscp/config/common.mk)
 
 # Call the proprietary setup
 $(call inherit-product-if-exists, vendor/oneplus/onyx/onyx-vendor.mk)
